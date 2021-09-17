a05e3ad750e0b8861154c16405b332a7fd105ac6
Braxten <braxten.d312@stu.nebo.edu>
2021-09-17 08:24:31 -0600

    code changes

--- Prototype 1 - tank sim/Assets/Scripts/camerafollow.cs
+++ Prototype 1 - tank sim/Assets/Scripts/camerafollow.cs
@@ -6,13 +6,11 @@ public class camerafollow : MonoBehaviour
 {
   public GameObject tank;
   private Vector3 offset = new Vector3(0,25,-50);
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
     {
-     transform.position = tank.transorm.position * offset;   
+     transform.position = tank.transform.position * offset;   
     }
 }
