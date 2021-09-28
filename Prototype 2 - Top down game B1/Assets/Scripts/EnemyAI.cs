ac6c2d16b4e2297407d11347061681862980f897
Braxten <braxten.d312@stu.nebo.edu>
2021-09-28 09:22:56 -0600

    worked on EmemyAI code

--- Prototype 2 - Top down game B1/Assets/Scripts/EnemyAI.cs
+++ Prototype 2 - Top down game B1/Assets/Scripts/EnemyAI.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    public Transform Player;
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 direction = Player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
     }
 
-    void OnTriggerEnter2D(Collider2D other);
+    // pccurs at fixed intervals (certain framerate)
+    void FixedUpdate()
     {
-        if(Other.gameobject.CompareTag("Projectile"))
+        MoveEmemy(movement);
+    }
+
+    void MoveEmemy(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position * (direction * moveSpeed * Time.deltaTime));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Projectile 1")) 
         {
-            Destroy(gameobject);
+             Destroy(gameObject);
         }
     }
 }
